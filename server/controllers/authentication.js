5aebe7fdb213ec2f95fb198c2b6b5d44f3ff10f2
Cody Condon <codycondon50@gmail.com>
2016-03-31 17:58:26 -0400

    returning jwt token when user signs up

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
 
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 exports.signup = function(req,res,next) {
   const email = req.body.email;
   const password = req.body.password;
@@ -23,7 +29,7 @@ exports.signup = function(req,res,next) {
     user.save(function(err) {
       if (err) {return next(err);}
         //Response to request indicating the user was created
-        res.json({success: true});
+        res.json({token: tokenForUser(user)});
     });
   });
 
