5b40b7d570a51852ab4f7e5dc3bb5d769f5f53a8
Cody Condon <codycondon50@gmail.com>
2016-03-31 16:02:28 -0400

    json rendering in browser

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
+
 
 //App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+router(app);
 
 //Server setup
 const port = process.env.PORT || 3090;
